34e616dcf132f561c071e58e07e09c39c5e88835
LEUNSU <lesu002@naver.com>
2024-05-28 14:23:34 +0900

    20240528

--- part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
+++ part2/win/SmartHomeMonitor/Views/RealTimeControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using LiveChartsCore.SkiaSharpView.Extensions;
+﻿using LiveChartsCore.SkiaSharpView.Drawing;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.VisualElements;
 using MahApps.Metro.Controls;
 using MQTTnet;
 using MQTTnet.Client;
@@ -6,22 +9,16 @@ using Newtonsoft.Json;
 using SmartHomeMonitoringApp.Logics;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace SmartHomeMonitoringApp.Views
 {
@@ -35,31 +32,30 @@ namespace SmartHomeMonitoringApp.Views
             InitializeComponent();
 
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 1); // 1초마다
+            timer.Interval = new TimeSpan(0, 0, 1);   // 1초마다
             timer.Tick += Timer_Tick;
             timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            LblSensingDt.Content = DateTime.Now.ToString();
+            LblSensingDt.Content = DateTime.Now.ToString("yyyy년 MM월 dd일 HH:mm:ss");
         }
 
         private async void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected) 
-            { // 이미 다른 화면에서 MQTT를 연결했다면...
+            // MQTT 초기화 설정
+            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+            {   // 이미 다른 화면에서 MQTT를 연결한 경우
                 Commons.MQTT_CLIENT.ApplicationMessageReceivedAsync += MQTT_CLIENT_ApplicationMessageReceivedAsync;
             }
-            else
+            else // 한번도 연결한 적이 없으면
             {
-                // 한 번도 연결한 적이 없으면
                 var mqttFactory = new MqttFactory();
                 Commons.MQTT_CLIENT = mqttFactory.CreateMqttClient();
                 var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(Commons.BROKERHOST).Build();
                 await Commons.MQTT_CLIENT.ConnectAsync(mqttClientOptions, CancellationToken.None);
                 Commons.MQTT_CLIENT.ApplicationMessageReceivedAsync += MQTT_CLIENT_ApplicationMessageReceivedAsync;
-
 
                 var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter(
                     f =>
@@ -76,7 +72,21 @@ namespace SmartHomeMonitoringApp.Views
             Debug.WriteLine(payload);
             UpdateChart(payload);
 
-            return Task.CompletedTask;
+            return Task.CompletedTask; // Async에서 Task값을 넘겨주려면 이렇게
+        }
+
+        // 앵귤러 차트를 위한 변수, 속성 선언
+        public IEnumerable<ISeries> HumidSeries { get; set; }
+
+        public IEnumerable<VisualElement<SkiaSharpDrawingContext>> VisualElements { get; set; } // 차트 만들어줌
+
+        public NeedleVisual Needle { get; set; }    // 바늘모양을 만들어줌
+
+        private static void SetStyle(
+        double sectionsOuter, double sectionsWidth, PieSeries<ObservableValue> series)
+        {
+            series.OuterRadiusOffset = sectionsOuter;
+            series.MaxRadialColumnWidth = sectionsWidth;
         }
 
         private void UpdateChart(string payload)
@@ -95,11 +105,56 @@ namespace SmartHomeMonitoringApp.Views
                     series =>
                     {
                         series.MaxRadialColumnWidth = 50;
-                        series.DataLabelsSize = 50;
+                        series.DataLabelsSize = 40;
                     }
-                  ));
-                ChtDinningTemp.Series = tempVal;
+                ));
+                ChtLivingTemp.Series = ChtDiningTemp.Series = ChtBedTemp.Series = ChtBathTemp.Series = tempVal; // 차트 네개 다 같은 값을 할당
+
+
+                var sectionsOuter = 130;
+                var sectionsWidth = 20;
+
+                // 습도차트 값 할당
+                HumidSeries = GaugeGenerator.BuildAngularGaugeSections(
+                    new GaugeItem(humid,
+                    s => SetStyle(sectionsOuter, sectionsWidth, s))
+                );
+                ChtLivingHumid.Series = ChtDiningHumid.Series = ChtBedHumid.Series = ChtBathHumid.Series = HumidSeries;
+
+                // 습도를 나타낼 앵귤러차트를 초기화
+                // 앵귤러 차트를 그리기 위한 속성들 51 ~ 62행
+                Needle = new NeedleVisual { Value = humid };   // 차트 바늘 45로 초기화
+                VisualElements = new VisualElement<SkiaSharpDrawingContext>[]
+                {
+                new AngularTicksVisual
+                {   // 앵귤러 차트를 꾸미는 속성
+                    LabelsSize = 12,
+                    LabelsOuterOffset = 15,
+                    OuterOffset = 65,
+                    TicksLength = 15    // 차트 실선 길이
+                },
+                Needle
+                };
+
+                // 위에서 만든 화면 디자인을 차트에 적용
+                ChtLivingHumid.VisualElements = ChtDiningHumid.VisualElements = VisualElements;
+                ChtBedHumid.VisualElements = ChtBathHumid.VisualElements = VisualElements;
             });
+        }
+
+        private void BtnWarning_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'conrtol':'warning'} ");
+        }
+
+        private void BtnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'conrtol':'normal'} ");
+        }
+
+        private void BtnOff_Click(object sender, RoutedEventArgs e)
+        {
+            Commons.MQTT_CLIENT.PublishStringAsync("pknu/rcv/", "{'conrtol':'off'} ");
         }
     }
 }
